27aa9469bf62db62b3d518d8b414942e7e7a843f
Moises56 <moiavil56@gmail.com>
2025-03-21 15:51:47 -0600

    map

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Allow requests from Angular frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // If you need to send cookies or auth headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
